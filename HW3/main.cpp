0b20e563b2ee9867c922c1667895b41695bf9b60
jonathanku0313 <61576551+jonathanku0313@users.noreply.github.com>
2020-04-13 00:59:34 +0800

    Origin

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -22,7 +22,7 @@
 #define FXOS8700Q_M_CTRL_REG2 0x5C
 #define FXOS8700Q_WHOAMI_VAL 0xC7
 
-I2C i2c( PTD9,PTD8);
+I2C i2c(PTD9,PTD8);
 Serial pc(USBTX, USBRX);
 DigitalOut redLED(LED1);
 InterruptIn button(SW2);
@@ -34,22 +34,22 @@ void FXOS8700CQ_writeRegs(uint8_t * data, int len);
 
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread thread;
-Ticker time_blink;
+Ticker time_up;
 
-void blink_led() {
+void blink() {
     redLED = !redLED;
 }
 
 void logger() {
    Timer t10;
 
-   time_blink.attach(&blink_led, 0.1);
+   time_up.attach(&blink, 0.2);
    pc.baud(115200);
-   uint8_t who_am_i, data[2], res[6];
+   uint8_t data[2], res[6];
    int16_t acc16;
-   float t[3]; //staionary vector
-   float r[3]; //shifting vector
-   int cnt = 0;
+   float s[3]; //stationary
+   float d[3]; //delta
+   int initial = 0;
 
    // Enable the FXOS8700Q
    FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
@@ -61,58 +61,59 @@ void logger() {
    while (t10.read() <= 11){
       FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
 
-    if(cnt == 0){
+    if(initial == 0){
         acc16 = (res[0] << 6) | (res[1] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        t[0] = ((float)acc16) / 4096.0f;
-        r[0] = ((float)acc16) / 4096.0f;
+        s[0] = ((float)acc16) / 4096.0f;
+        d[0] = ((float)acc16) / 4096.0f;
 
         acc16 = (res[2] << 6) | (res[3] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        t[1] = ((float)acc16) / 4096.0f;
-        r[1] = ((float)acc16) / 4096.0f;
+        s[1] = ((float)acc16) / 4096.0f;
+        d[1] = ((float)acc16) / 4096.0f;
 
         acc16 = (res[4] << 6) | (res[5] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        t[2] = ((float)acc16) / 4096.0f;
-        r[2] = ((float)acc16) / 4096.0f;
-        cnt++;
+        s[2] = ((float)acc16) / 4096.0f;
+        d[2] = ((float)acc16) / 4096.0f;
+        initial++;
     }else{
         acc16 = (res[0] << 6) | (res[1] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        r[0] = ((float)acc16) / 4096.0f;
+        d[0] = ((float)acc16) / 4096.0f;
 
         acc16 = (res[2] << 6) | (res[3] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        r[1] = ((float)acc16) / 4096.0f;
+        d[1] = ((float)acc16) / 4096.0f;
 
         acc16 = (res[4] << 6) | (res[5] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        r[2] = ((float)acc16) / 4096.0f;
-        cnt++;
+        d[2] = ((float)acc16) / 4096.0f;
     }  
 
-    float cosx = (t[0]*r[0]+t[1]*r[1]+t[2]*r[2]) / (sqrt(t[0]*t[0]+t[1]*t[1]+t[2]*t[2])*sqrt(r[0]*r[0]+r[1]*r[1]+r[2]*r[2]));
+    // inner product
+    float cosx = (s[0]*d[0]+s[1]*d[1]+s[2]*d[2]) / (sqrt(s[0]*s[0]+s[1]*s[1]+s[2]*s[2])*sqrt(d[0]*d[0]+d[1]*d[1]+d[2]*d[2]));
     float cos45 = 1 / sqrt(2);
 
-    if(cosx <= cos45)
-        pc.printf("%f\r\n%f\r\n%f\r\n%d\r\n", r[0], r[1], r[2], 1);
-    else
-        pc.printf("%f\r\n%f\r\n%f\r\n%d\r\n", r[0], r[1], r[2], 0);
+    if(cosx <= cos45){
+        pc.printf("%f\r\n%f\r\n%f\r\n%d\r\n", d[0], d[1], d[2], 1);
+    }else{
+        pc.printf("%f\r\n%f\r\n%f\r\n%d\r\n", d[0], d[1], d[2], 0);
+    }
 
     wait(0.1);
   }
    
-   time_blink.detach();
+   time_up.detach();
    t10.stop();
 }
-int main() {
+int main(){
     redLED = 1;
 
     thread.start(callback(&queue, &EventQueue::dispatch_forever));
